ae9c1599b405b3f3b4c8132969e5c4bd2a8f989d
Christopher Herman <crherman7@gmail.com>
2019-03-26 20:59:25 -0400

    Added api calls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import * as BooksAPI from "./BooksAPI";
 
 class App extends Component {
+  state = {
+    currentlyReading: [],
+    read: [],
+    wantToRead: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({
+        currentlyReading: books.filter(book => book.shelf === "read"),
+        read: books.filter(book => book.shelf === "read"),
+        wantToRead: books.filter(book => book.shelf === "wantToRead")
+      }));
+    });
+  }
+
+  onAddBook = (shelf, book) => {
+    this.setState(state => ({
+      [shelf]: state[shelf].concat([book])
+    }));
+  };
+
+  onRemoveBook = (shelf, book) => {
+    this.setState(state => ({
+      [shelf]: state[shelf].filter(b => b.id !== book.id)
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Navbar />
       </div>
     );
